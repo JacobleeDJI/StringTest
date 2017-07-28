a83fc7fc7fbe8d07b369c9777e0c97be07bda486
Jacoblee <jacobleeloveru@gmail.com>
2017-07-28 11:55:37 +0800

    new code

--- StringTest/main.cpp
+++ StringTest/main.cpp
@@ -10,13 +10,25 @@
 #include <string>
 using namespace std;
 
+//int main()
+//{
+//    string str = "qweasdzxc";
+//    for (auto &c : str ) { //使用引用，改变的是字符串str的值
+//        c = 'X';
+//        
+//    }
+//    cout << str << endl;
+//    return 0;
+//}
 int main()
 {
-    string str = "qweasdzxc";
-    for (auto &c : str ) { //使用引用，改变的是字符串str的值
-        c = 'X';
-        
+    string str;
+    while (getline(cin, str)) {
+        for (auto &c : str) {
+            if (!ispunct(c)) {
+                cout << c;
+            }
+        }
     }
-    cout << str << endl;
     return 0;
 }
